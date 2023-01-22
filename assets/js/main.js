62a2dd2bbf92a0253ce18c7d35ac87e423cc50a9
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-01-22 14:37:46 -0300

    Meu ponto zero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,26 @@
+const ol = document.getElementById('pokemonList')
+const offset = 0
+const limit = 10
 
-console.log('Sucesso!')
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon  ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+
+            <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+
+pokeApi.getPokemons().then((pokemons = []) => {
+        ol.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    })
